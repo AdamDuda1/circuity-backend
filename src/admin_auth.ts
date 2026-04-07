a76885dadd7dd46e15480950c2e364ca22843474
Adam Duda <adam.duda@post.com>
2026-04-07 23:06:18 +0200

    export JSON + fix routes

--- src/admin_auth.ts
+++ src/admin_auth.ts
@@ -66,7 +66,7 @@ export function auth(
 ) {
 	const header = req.headers['authorization'];
 	if (!header || !header.startsWith('Bearer ')) {
-		return res.status(401).json({error: 'Missing bearer token'});
+		return res.status(401).json({error: 'Missing bearer token!'});
 	}
 
 	const token = header.split(' ')[1];
